4e7b379a6fedcbe16181fdc6746c7148a512c2d0
Alexander <sanjzamik10000@mail.ru>
2023-03-20 21:14:29 +0700

    adding comments

--- Lab_1/ConsoleOutput.cpp
+++ Lab_1/ConsoleOutput.cpp
@@ -9,7 +9,7 @@ void ConsoleOutput::printFileCreated(QString filePath, qint64 fileSize)
 {
 	qDebug() << "File was created:" << filePath << ", size:" << fileSize << "bytes";
 }
-
+//Метод вывода информации о добавлении файла в мониторинг
 void ConsoleOutput::printfileAddedToWatcher(QString filePath, qint64 fileSize)
 {
 	qDebug() << "File was added to watcher:" << filePath << ", size:" << fileSize << "bytes";
